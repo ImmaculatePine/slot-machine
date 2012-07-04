1fd565de5df57520762b542ef6565e41f8a4196b
Alexander Borovykh <immaculate.pine@gmail.com>
2012-07-04 17:11:33 +0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlotMachine::Application.config.secret_token = '99a4cfc5513f207a15d279e0ec968b82209bb8c67ca0af1e0cca7a645586fc703012c7e898bc21bb67765a408b1d9dabf874521e13888952ba646ba805a65044'
