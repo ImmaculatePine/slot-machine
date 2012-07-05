7d6d1ec329a614211a1fa8d093df0522441d80be
Alexander Borovykh <immaculate.pine@gmail.com>
2012-07-05 15:22:57 +0400

    press_button method added to Machine model to generate random spining of reels.

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,8 +2,21 @@ class Machine
   include ActiveModel::Conversion
   extend ActiveModel::Naming
   
-  attr_accessor :name, :reels
+  LINES_QUANTITY = 3
+  
+  attr_accessor :name, :reels, :result
 
+  def press_button
+    self.result = []
+    self.reels.each do |reel|
+      # Generate random shift to move every reel
+      shift = rand(0..reel.length-1)
+      # Adding 3x5 array to result
+      # 2 reels are concatenated becuase reel is a circle
+      self.result << (reel+reel)[shift..shift+LINES_QUANTITY]
+    end
+  end
+  
   private
     def initialize(attributes = {})
       attributes.each do |name, value|
